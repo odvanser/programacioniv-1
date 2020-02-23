0b67a8e235138fd5fc8320a0ce8c51d104ec3a2c
freddycastro11 <Freddyenrique2000@gmail.com>
2020-02-23 14:31:15 -0600

    xd

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded",e=>{
     document.addEventListener("submit",event=>{
         event.preventDefault();
-        
-        
+        let $resp = document.querySelector("#lblSaludo"),
+            nombre = document.querySelector("#txtNombre").value;
+        $resp.innerHTML = 'Iniciando Peticion al server...';
+
+        fetch(`saludo.php?nombre=${nombre}`).then(resp=>resp.text()).then(resp=>{
+            //$resp.innerHTML = `Hola ${nombre} ${resp}`;
+            $resp.innerHTML = resp;
+        });
     });
 });
---
+++ saludo.php
@@ -0,0 +1,7 @@
+<?php
+    /** Definir la zona horaria  */
+    date_default_timezone_set('America/El_salvador');
+
+    $nombre = $_GET['nombre'];
+    echo 'Hola '. $nombre .' desde el servidor '.date('d-m-Y H:i:s');
+?> 
