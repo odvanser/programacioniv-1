59b85cdd3759e5f95773c5603024698f3659e261
freddycastro11 <Freddyenrique2000@gmail.com>
2020-02-23 14:36:53 -0600

    xd

--- saludo.php
+++ saludo.php
@@ -2,6 +2,7 @@
     /** Definir la zona horaria  */
     date_default_timezone_set('America/El_salvador');
 
+    
     $nombre = $_GET['nombre'];
     echo 'Hola '. $nombre .' desde el servidor '.date('d-m-Y H:i:s');
 ?> 
