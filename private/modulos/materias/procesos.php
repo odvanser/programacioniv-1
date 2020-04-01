681e207a44c9acefd4e92a5e574c4ecdf322fb2c
freddycastro11 <Freddyenrique2000@gmail.com>
2020-04-01 10:35:34 -0600

    guardar materias

--- private/modulos/materias/procesos.php
+++ private/modulos/materias/procesos.php
@@ -1,23 +1,23 @@
 <?php 
 include('../../config/config.php');
-$docente = new docente($conexion);
+$materia = new materia($conexion);
 
 $proceso = '';
 if( isset($_GET['proceso']) && strlen($_GET['proceso'])>0 ){
 	$proceso = $_GET['proceso'];
 }
-$docente->$proceso( $_GET['docente'] );
-print_r(json_encode($docente->respuesta));
+$materia->$proceso( $_GET['materia'] );
+print_r(json_encode($materia->respuesta));
 
-class docente{
+class materia{
     private $datos = array(), $db;
     public $respuesta = ['msg'=>'correcto'];
     
     public function __construct($db){
         $this->db=$db;
     }
-    public function recibirDatos($docente){
-        $this->datos = json_decode($docente, true);
+    public function recibirDatos($materia){
+        $this->datos = json_decode($materia, true);
         $this->validar_datos();
     }
     private function validar_datos(){
@@ -27,8 +27,8 @@ class docente{
         if( empty($this->datos['nombre']) ){
             $this->respuesta['msg'] = 'por favor ingrese el Nombre';
         }
-        if( empty($this->datos['direccion']) ){
-            $this->respuesta['msg'] = 'por favor ingrese la Direccion';
+        if( empty($this->datos['modalidad']) ){
+            $this->respuesta['msg'] = 'por favor ingrese la Modalidad';
         }
 
         $this->almacenar_docente();
@@ -37,40 +37,40 @@ class docente{
         if( $this->respuesta['msg']==='correcto' ){
             if( $this->datos['accion']==='nuevo' ){
                 $this->db->consultas('
-                    INSERT INTO docentes (codigo,nombre,direccion,telefono) VALUES(
+                    INSERT INTO materias (codigo,nombre,modalidad,informacion) VALUES(
                         "'. $this->datos['codigo'] .'",
                         "'. $this->datos['nombre'] .'",
-                        "'. $this->datos['direccion'] .'",
-                        "'. $this->datos['telefono'] .'"
+                        "'. $this->datos['modalidad'] .'",
+                        "'. $this->datos['informacion'] .'"
                     )
                 ');
                 $this->respuesta['msg'] = 'Registro insertado correctamente';
             } else if( $this->datos['accion']==='modificar' ){
                 $this->db->consultas('
-                    UPDATE docentes SET
+                    UPDATE materias SET
                         codigo      = "'. $this->datos['codigo'] .'",
                         nombre      = "'. $this->datos['nombre'] .'",
-                        direccion   = "'. $this->datos['direccion'] .'",
-                        telefono    = "'. $this->datos['telefono'] .'"
-                    WHERE idDocente = "'. $this->datos['idDocente'] .'"
+                        modalidad   = "'. $this->datos['modalidad'] .'",
+                        informacion    = "'. $this->datos['informacion'] .'"
+                    WHERE idMateria = "'. $this->datos['idMateria'] .'"
                 ');
                 $this->respuesta['msg'] = 'Registro actualizado correctamente';
             }
         }
     }
-    public function buscarDocente($valor = ''){
+    public function buscarMateria($valor = ''){
         $this->db->consultas('
-            select docentes.idDocente, docentes.codigo, docentes.nombre, docentes.direccion, docentes.telefono
-            from docentes
-            where docentes.codigo like "%'. $valor .'%" or docentes.nombre like "%'. $valor .'%" or docentes.direccion like "%' . $valor .'%"
+            select materias.idMateria, materias.codigo, materias.nombre, materias.modalidad, materias.informacion
+            from materias
+            where materias.codigo like "%'. $valor .'%" or materias.nombre like "%'. $valor .'%" or materias.modalidad like "%' . $valor .'%"
         ');
         return $this->respuesta = $this->db->obtener_data();
     }
-    public function eliminarDocente($idDocente = 0){
+    public function eliminarMateria($idMateria = 0){
         $this->db->consultas('
-            DELETE docentes
-            FROM docentes
-            WHERE docentes.idDocente="'.$idDocente.'"
+            DELETE materias
+            FROM materias
+            WHERE materias.idMateria="'.$idMateria.'"
         ');
         return $this->respuesta['msg'] = 'Registro eliminado correctamente';
     }
--- public/vistas/materias/materias.js
+++ public/vistas/materias/materias.js
@@ -13,7 +13,7 @@ var appmateria = new Vue({
         },
         methods:{
             guardarMateria:function(){
-                fetch(`private/modulos/materias/procesos.php?proceso=recibirDatos&materias=${JSON.stringify(this.materia)}`).then(resp => resp.json()).then(resp=>{
+                fetch(`private/modulos/materias/procesos.php?proceso=recibirDatos&materia=${JSON.stringify(this.materia)}`).then(resp => resp.json()).then(resp=>{
                     this.materia.msg = resp.msg;
                     this.materia.idMateria = 0;
                     this.materia.codigo = '';
